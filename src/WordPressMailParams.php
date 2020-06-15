f83cc4af2b7934da79899695ee4dc5f62ac53c00
Rumur <rumur.dev@gmail.com>
2020-06-15 17:36:42 +0200

    Added WordPessMailParams DTO

---
+++ src/WordPressMailParams.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Rumur\WPMailer;
+
+class WordPressMailParams
+{
+    public $to;
+    public $subject;
+    public $body;
+    public $headers;
+    public $attachments;
+    /** @var null|string */
+    public $locale;
+
+    /**
+     * WordPressMailParams constructor.
+     *
+     * @param string $to
+     * @param string $subject
+     * @param string $body
+     * @param array $headers
+     * @param array $attachments
+     * @param null|string $locale
+     */
+    public function __construct(
+        string $to, string $subject, string $body,
+        array $headers = [], array $attachments = [],
+        ?string $locale = null
+    )
+    {
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->body = $body;
+        $this->headers = $headers;
+        $this->attachments = $attachments;
+        $this->locale = $locale;
+    }
+}
